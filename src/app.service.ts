d14719fdccbc3fa878530be7e276b767f6e8dadd
AmineOuerghi07 <MohamedAmineOuerghi07@gmail.com>
2024-11-06 11:59:13 +0100

    gemini v1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private genAI : any
+  private genAIFlashModel : any
+  constructor(private readonly config: ConfigService)
+  {
+     this.genAI = new GoogleGenerativeAI(this.config.get("API_KEY"))
+    this.genAIFlashModel = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
+  }
+
+  async getPromptResponse(prompt : string)
+  { 
+    const result = await this.genAIFlashModel.generateContent(prompt)
+    const response = await result.response
+    const text : string =response.text()
+    let jsonText = text.slice(7)
+    jsonText = jsonText.slice(0,jsonText.indexOf("```"))
+    console.log(jsonText)
+    //console.log(text)
+    const jsonResponse = JSON.parse(jsonText)
+    return jsonResponse
   }
 }
