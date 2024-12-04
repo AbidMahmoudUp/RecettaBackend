510427a13a0d56a5d4f336e3b6b28460bc0b49f1
Mahmoud Abid <mahmoudabidup@gmail.com>
2024-12-04 14:11:33 +0100

    uploading the genrated image to multer

--- src/stable-diffusion-ai/stable-diffusion-ai.controller.ts
+++ src/stable-diffusion-ai/stable-diffusion-ai.controller.ts
@@ -4,14 +4,27 @@ import { CreateStableDiffusionAiDto } from './dto/create-stable-diffusion-ai.dto
 import { UpdateStableDiffusionAiDto } from './dto/update-stable-diffusion-ai.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createReadStream } from 'fs';
+import { diskStorage } from 'multer';
 
 @Controller('api/stable-diffusion-ai')
 export class StableDiffusionAiController {
   constructor(private readonly stableDiffusionAiService: StableDiffusionAiService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
-  async genrateImage(@Body() data:CreateStableDiffusionAiDto , @Res() res :any  ){
+  @UseInterceptors(FileInterceptor('file',{
+    storage: diskStorage({
+      destination:'./assets',
+      filename:(req,file,callback) =>
+      {
+        const uniqueName = Date.now()+ '+' + Math.round(Math.random() *1e9)
+        const extentionfile = file.originalname.split('.').pop()
+        const filename = `${file.fieldname}-${uniqueName}.${extentionfile}`
+        callback(null,filename)
+      },
+
+    }),
+  }),
+)  async genrateImage(@Body() data:CreateStableDiffusionAiDto , @Res() res :any  ){
     const filePath = await this.stableDiffusionAiService.generateImage(data)
     const file  = createReadStream(filePath)
     file.pipe(res)
